ee30b94a6a40a3cdb310df7c97613ba294b6f89c
arnorien <arno.rieux@gmail.com>
2026-08-15 17:32:57 +0200

    Modification du fichier blink et ajout de claude code

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,6 +1,9 @@
 return {
   {
     "saghen/blink.cmp",
+    dependencies = {
+      "erooke/blink-cmp-latex",
+    },
     opts = {
       completion = {
         trigger = {
@@ -8,6 +11,22 @@ return {
           show_on_keyword = false,
           -- Active l'affichage UNIQUEMENT pour les commandes (comme le \ de LaTeX)
           show_on_trigger_character = true,
+        },
+      },
+      sources = {
+        -- Ajoute la source "latex" au filetype tex, en plus des sources par défaut
+        per_filetype = {
+          tex = { inherit_defaults = true, "latex" },
+        },
+        providers = {
+          latex = {
+            name = "Latex",
+            module = "blink-cmp-latex",
+            opts = {
+              -- false : insère le symbole (ex. α) plutôt que la commande (\alpha)
+              insert_command = false,
+            },
+          },
         },
       },
     },
